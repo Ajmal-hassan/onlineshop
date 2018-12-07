21737be0db8fb898dde7443c891d1ccaaa573ea0
Ajmal hassan <ajmalhassan651@gmail.com>
2018-12-07 11:21:34 +0500

    Partation all site

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
   root to: 'page#index'
+  get 'profile', to: 'admins#index'
+  get 'orders', to: 'orders#index'
+  get 'add_product', to: 'products#new'
 end
